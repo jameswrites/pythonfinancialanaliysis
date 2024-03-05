2e38a4163ad92515c47a0650152929a95df6fb26
James Mitchell <122945368+jameswrites@users.noreply.github.com>
2024-03-05 15:18:14 +1100

    Not sure if this commit is going to the right place

---
+++ financialanalysis.py
@@ -0,0 +1,24 @@
+import requests
+import matplotlib.pyplot as plt
+
+# Define your FMP API key
+api_key = '194b4b2704bf876cb92927b427b518d1'
+
+# Define the endpoint for fetching income statement
+# endpoint = 'https://financialmodelingprep.com/api/v3/financials/income-statement/AAPL'
+
+
+company = "AAPL"
+years = 10
+
+income_statement = requests.get(f"https://financialmodelingprep.com/api/v3/income-statement/{company}?limit={years}&apikey={api_key}")
+income_statement = income_statement.json()
+
+revenues = list(reversed([income_statement[i]['revenue'] for i in range (len(income_statement))]))
+profits = list(reversed([income_statement[i]['grossProfit'] for i in range (len(income_statement))]))
+
+
+plt.plot(revenues, label="Revenue")
+plt.plot(profits, label="Profit")
+plt.legend(loc="upper left")
+plt.show()
